25d1a24deb3b36c8184b740ae48f18a6a0e2bef9
Augusto Viggiano <112136936+AugusViggia@users.noreply.github.com>
2024-10-07 16:36:31 -0300

    redeploy

--- client/src/components/ProductCart/ProductCart.jsx
+++ client/src/components/ProductCart/ProductCart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useProductHandlers } from "../../handlers/productHandlers";
 import DeleteProductModal from '../Modals/DeleteProductModal';
